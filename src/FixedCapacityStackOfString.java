5aaabfdb1d83cd08d1917097fdd820d1538ae974
majiddaeinejad <majid.daeinejad@gmail.com>
2023-06-01 11:53:03 +0330

    Allow garbage collector to reclaim the null cell

--- src/FixedCapacityStackOfString.java
+++ src/FixedCapacityStackOfString.java
@@ -18,6 +18,8 @@ public class FixedCapacityStackOfString implements StackOfString {
 
     @Override
     public String pop() {
-        return s[--N];
+        String item = s[--N];
+        s[N] = null;
+        return item;
     }
 }
