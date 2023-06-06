1137c3a1e9efe48a75bc93759a4240e4a1414e74
majiddaeinejad <majid.daeinejad@gmail.com>
2023-06-06 19:47:52 +0330

    Implementation of ShellSort

---
+++ src/sort/elementary/ShellSort.java
@@ -0,0 +1,43 @@
+package sort.elementary;
+
+import edu.princeton.cs.algs4.Shell;
+
+public class ShellSort {
+    public static void sort(Comparable[] a) {
+        int n = a.length;
+        // 3x+1 increment sequence:  1, 4, 13, 40, 121, 364, 1093, ...
+        int h = 1;
+        while (h < n / 3) h = 3 * h + 1;
+
+        while (h >= 1) {
+            // h-sort the array
+            for (int i = h; i < n; i++) {
+                for (int j = i; j >= h && less(a[j], a[j - h]); j -= h) {
+                    exch(a, j, j - h);
+                }
+            }
+            h /= 3;
+        }
+    }
+
+    public static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    public static void exch(Comparable a[], int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        String[] input = "S O R T E X A M P L E".split(" ");
+        sort(input);
+        for (int i = 0; i < input.length; i++) {
+            System.out.print(input[i]);
+        }
+
+    }
+
+
+}
